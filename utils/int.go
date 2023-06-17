a4e881eaf472325289071f0d29c4424a098036d7
shuk <biz.shuk@gmail.com>
2023-06-17 23:09:11 +0800

    int pointer

---
+++ utils/int.go
@@ -0,0 +1,25 @@
+package utils
+
+func IntPointer(i int) *int {
+	return &i
+}
+
+func Int32Pointer(i int32) *int32 {
+	return &i
+}
+
+func Int64Pointer(i int64) *int64 {
+	return &i
+}
+
+func UintPointer(i uint) *uint {
+	return &i
+}
+
+func Uint32Pointer(i uint32) *uint32 {
+	return &i
+}
+
+func Uint64Pointer(i uint64) *uint64 {
+	return &i
+}
