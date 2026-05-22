3b5e85917eb759e3754ffe2ef370870ab13a5f5c
shuk.liu <biz.shuk@gmail.com>
2026-05-23 01:08:59 +0800

    feat(versioning): add major subcommand

---
+++ cmd/versioning/cmd/major.go
@@ -0,0 +1,31 @@
+package cmd
+
+import (
+	"fmt"
+
+	"github.com/spf13/cobra"
+)
+
+var majorCmd = &cobra.Command{
+	Use:   "major",
+	Short: "Increment major version",
+	Long:  `Increments the major version and resets minor and patch to 0.`,
+	RunE: func(cmd *cobra.Command, args []string) error {
+		v, err := ReadVersion()
+		if err != nil {
+			return err
+		}
+		v.Major++
+		v.Minor = 0
+		v.Patch = 0
+		if err := WriteVersion(v); err != nil {
+			return err
+		}
+		fmt.Println(v)
+		return nil
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(majorCmd)
+}
